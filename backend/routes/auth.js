8b0e9fa0e514bcf810a58bd759fb63fcfd3eb283
djayMehrotra <mehrotradj23@gmail.com>
2021-11-20 12:45:18 +0530

    #50 user able to login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -11,22 +11,28 @@ const JWT_SECRET = "Dannyisagoodb$oy";
 
 // Create a user using: POST "/api/auth/createuser". No login required
 router.post('/createuser', [
-    body('name', 'Enter a valid name').isLength({min: 3}),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be atleast 5 characters').isLength({min: 5}),
-], async (req, res)=>{
-    // Finds the validation errors in this request and wraps them in an object with handy functions
-    // check whether the user with this email already exists 
-    try {
-      //Logger
-      //SQS
-      // using if in case db error occurs
-    let user = await User.findOne({email: req.body.email});
+  body('name', 'Enter a valid name').isLength({ min: 3 }),
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  // check whether the user with this email already exists 
+  try {
+    //Logger
+    //SQS
+    // using if in case db error occurs
+    let user = await User.findOne({ email: req.body.email });
     console.log(user);
-    if(user){
-      return res.status(400).json({error: "Sorry user already exists with this email id"})
+    if (user) {
+      return res.status(400).json({ error: "Sorry user already exists with this email id" })
     }
-    
+
     const salt = await bcrypt.genSalt(10);
     secPass = await bcrypt.hash(req.body.password, salt);
     const saltRounds = 10;
@@ -39,34 +45,74 @@ router.post('/createuser', [
 
     // Create a new user
     user = await User.create({
-        name: req.body.name,
-        password: secPass,
-        email: req.body.email
-      })
+      name: req.body.name,
+      password: secPass,
+      email: req.body.email
+    })
 
-      const data = {
-        user:{
-          id: user.id
-        }
+    const data = {
+      user: {
+        id: user.id
       }
+    }
 
-      /* const jwtData = jwt.sign(data, JWT_SECRET);
-      console.log(jwtData); */
-      
-      const authtoken = jwt.sign(data, JWT_SECRET);
+    const jwtData = jwt.sign(data, JWT_SECRET);
+    console.log(jwtData);
 
-      //res.json({authtoken});
-      res.json(user);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send("Some error occured");
+    //res.json({authtoken});
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Create User :: Internal server error occured");
+  }
+
+});
+
+// Login a user using: POST "/api/auth/login". No login required
+router.post('/login', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'password cannot be blank').exists(),
+], async (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("hai error");
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    console.log(user);
+    if (!user) {
+      return res.status(400).json({ error: "Please try to login with existing user" })
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare) {
+      return res.status(400).json({ error: "Please try to login with correct password" })
+    }
+
+    const payload = {
+      user: {
+        id: user.id
       }
+    }
+
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+    res.json({authtoken});
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Login :: Internal server error occured");
+  }
 
 });
 
 module.exports = router
 
-/* 
+/*
 http://localhost:3000/api/auth
 */
 
